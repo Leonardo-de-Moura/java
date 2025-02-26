c02d271d1a3c46a4d1aad102f8f85f2115daf9a1
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-26 12:50:12 -0300

    up

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("afonsao deu o rabao");
-        System.out.println(34+35);
+        System.out.println("hello world!");
+
+           System.out.println(34+35);
     }
 }
