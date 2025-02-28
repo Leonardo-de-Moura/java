b982e5fa2553f1b6bb325188ab653a5b4225bd01
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-28 10:13:58 -0300

    up

---
+++ tipos.java
@@ -0,0 +1,21 @@
+public class tipos {
+	public static void main(String args[]){
+		boolean um = true;
+		char fg = 'a';
+		byte cg = 12;
+		short s = 24;
+		int i = 123;
+		long l = 8900L;
+		float f = 3.1234F;
+		double d= 21345;
+
+		System.out.println("tipos: "+um);
+		System.out.println("tipos: "+ fg);
+		System.out.println("tipos: "+cg);
+		System.out.println("tipos: "+s);
+		System.out.println("tipos: "+i);
+		System.out.println("tipos: "+l);
+		System.out.println("tipos: "+f);
+		System.out.println("tipos: "+d);
+	}
+}
